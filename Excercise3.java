20cfb4c2ddbcbcd36ad26cdb58f18c537ab36eeb
Vignesh <Vignesh000129@gmail.com>
2023-04-19 14:51:23 +0530

    Excercise3 from w3school on Circle

---
+++ Excercise3.java
@@ -0,0 +1,25 @@
+/*
+ Write a Java program to create a class called "Circle" with a radius attribute. You can access and modify this attribute. Calculate the area and circumference of the circle
+*/
+class Circle{
+	int radius;
+	public Circle(){
+	}
+	public Circle(int radius){
+	this.radius = radius;
+	}
+	public double area(){
+	return 2*3.14*this.radius*this.radius;
+	}
+	public double circumference(){
+		return (2*3.14*this.radius);
+	}	
+}
+class Excercise3{
+	public static void main(String[] args){
+		Circle c = new Circle();
+		c.radius = 4;
+		System.out.println("radius is"+c.area());
+		System.out.ptintln("circumference is "+c.circumference());
+		}
+	}
